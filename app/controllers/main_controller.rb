6aa00ec8f1802d5a9bd3dc57c40920fbddfff22e
Andrew Burkhart <burkhartfc@icloud.com>
2022-05-22 19:45:10 -0700

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def index
+        flash.now[:notice] = "Login successful. Welcome to Chikin"
+        flash.now[:alert] = "Invalid email or password. You know why I'm not telling you which..."
     end
 end
