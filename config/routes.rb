02e6c15af46523c546fdc664f10fb2105ae3c038
Andrew Burkhart <burkhartfc@icloud.com>
2022-05-27 16:46:27 -0700

    Add basic User sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root "main#index"
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+  post 'sign_up', to: "registrations#create"
+
+  root "main#index"
 end
